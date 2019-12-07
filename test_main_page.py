2887cbfef135177595ca6e52149edb6dcb4db01c
irynavaskiv <irynavaskiv875@gmail.com>
2019-12-07 20:08:05 +0200

    add first test for main page

--- test_main_page.py
+++ test_main_page.py
@@ -1,23 +1,18 @@
 import pytest
-from selenium import webdriver
-from time import sleep
+
+from base_pytest import Base
 
 
-class Test01:
-
-    @pytest.fixture(autouse=True)
-    def prepare(self):
-        self.selenium = webdriver.Firefox()
-        self.selenium.get('{}'.format('https://shafa.ua/'))
-        sleep(2)
+class TestID001(Base):
 
     def test_mobile_menu_all_elements(self):
-        login = self.selenium.find_elements_by_xpath(
+        login = self.selenium.find_element_by_xpath(
             '/html/body/div/div[2]/div[1]/div/div[1]/header/div[2]/div/a[1]')
         login.click()
-        sleep(3)
+
         page_elements = self.selenium.find_elements_by_xpath(
             '/html/body/div[1]/div/div/div/div/div/div[2]')
         if len(page_elements) == 0:
             pytest.skip(msg='Page do not have any elements')
-        assert page_elements == 2
+
+        assert len(page_elements) == 1
