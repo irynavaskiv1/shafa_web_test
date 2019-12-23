5e8327c676986a81e5aacc35379229106b866ca6
irynavaskiv <irynavaskiv875@gmail.com>
2019-12-23 23:13:35 +0200

    add description to 9 test

--- test_009.py
+++ test_009.py
@@ -2,6 +2,17 @@ from base_pytest import Base
 
 
 class TestID009(Base):
+    """
+        TestCase: 009
+        Author: Iryna Vaskiv
+        Description: Check if main page have Google Play and App Store buttons.
+        Pre-condition: Login inside web/mobile interface.
+        Steps to reproduce:
+            1. Login into Shafa.ua
+            2. Find block with AGoogle Play and App Store buttons
+            3. Check len of this buttons
+        Expected result: Should be able 2 elements.
+    """
 
     def test_mobile_apps(self):
         test_outerwear = self.selenium.find_element_by_class_name(
