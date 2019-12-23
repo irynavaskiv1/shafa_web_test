5f23ecbf4e4c1becae0bd03b4cab9f20ebd34b13
irynavaskiv <irynavaskiv875@gmail.com>
2019-12-23 22:56:33 +0200

    add description to 8 test

--- test_008.py
+++ test_008.py
@@ -2,6 +2,23 @@ from base_pytest import Base
 
 
 class TestID008(Base):
+    """
+        TestCase: 008(first)
+        Author: Iryna Vaskiv
+        Description:
+            1. Check if page have social network field.
+            2. Check if page have all numbers of cities.
+            3. Check if page have all numbers of brand.
+        Pre-condition: Login inside web interface.
+        Steps to reproduce:
+            1. Login into Shafa.ua
+            2. Find correct box of button/buttons
+            3. Check child elements inside box
+        Expected result:
+            1. Should be 1 element.
+            2. Should be 20 elements.
+            3. Should b 18 elements.
+    """
 
     def test_we_are_on_social_networks(self):
         all_social_network = self.selenium.find_elements_by_xpath(
@@ -12,8 +29,8 @@ class TestID008(Base):
     def test_numbers_of_cities(self):
         city_index = self.selenium.find_element_by_class_name(
             'b-index-about__cities')
-        numbers_of_city = city_index.find_elements_by_tag_name('li')
-        assert len(numbers_of_city) == 20
+        numbers_of_cities = city_index.find_elements_by_tag_name('li')
+        assert len(numbers_of_cities) == 20
 
     def test_numbers_of_brands(self):
         brand_index = self.selenium.find_element_by_class_name(
@@ -23,6 +40,21 @@ class TestID008(Base):
 
 
 class TestID0081(Base):
+    """
+        TestCase: 008(second)
+        Author: Iryna Vaskiv
+        Description:
+            1. Check if outerwear block has all elements.
+            2. Check if skirt block has all elements.
+        Pre-condition: Login inside web interface.
+        Steps to reproduce:
+            1. Login into Shafa.ua
+            2. Find correct box of button/buttons
+            3. Check child elements inside box
+        Expected result:
+            1. Should be 9 element.
+            2. Should be 9 elements.
+    """
 
     def test_outerwear(self):
         test_outerwear = self.selenium.find_element_by_class_name(
