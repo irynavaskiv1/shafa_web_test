05ce41327b5dffa1b9636e2b8902a8c2ab438fa3
irynavaskiv <irynavaskiv875@gmail.com>
2020-01-08 22:24:10 +0200

    add 15 test

---
+++ test_0015.py
@@ -0,0 +1,45 @@
+from time import sleep
+from base_pytest import Base
+
+import pytest
+
+
+class TestID0015(Base):
+    """
+        TestCase: 0014(1, 2)
+        Author: Iryna Vaskiv
+        Description:
+            1. Check if user can see all brands into Shafa.ua site. Should use
+             'Все бренды' button inside 'Женщинам' block
+            2. Check if brands have all elements(16)
+        Pre-condition: Login inside web interface.
+        Steps to reproduce:
+            1. Click in 'Женщинам' button
+            2. Click in 'Все бренды' button
+            3. Check the header inside page
+        Expected result:
+            1. Should be 'Брендовая женская одежда' header.
+            2. Should be 16 elements
+    """
+
+    def test_all_women_site(self):
+        self.women_button().click()
+        self.all_woman_close().click()
+        sleep(5)
+        all_women_header = self.selenium.find_element_by_xpath(
+            '/html/body/div/div[2]/div[2]/div/div/div/div/div[1]/h1')
+        set_get_brands_header = set(all_women_header.text.split('\n'))
+        set_words = {'Женская одежда'}
+        assert set_words == set_get_brands_header
+
+    def test_all_women_elements(self):
+        self.women_button().click()
+        self.all_woman_close().click()
+        sleep(5)
+        container_brands = self.selenium.find_element_by_xpath(
+            '/html/body/div/div[2]/div[2]/div/div/div/div/div[2]')
+        child_elements = container_brands. \
+            find_elements_by_class_name('b-subcategories__item')
+        if len(child_elements) != 21:
+            pytest.skip(msg='Page do not have all elements')
+        assert len(child_elements) == 21
