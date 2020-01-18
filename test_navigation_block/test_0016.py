b7b85469394a010fec8344c8cb8c8e432d675c19
irynavaskiv <irynavaskiv875@gmail.com>
2020-01-19 00:54:43 +0200

    fix 16 test

--- test_navigation_block/test_0016.py
+++ test_navigation_block/test_0016.py
@@ -6,35 +6,36 @@ from sf_setup_helper.base_pytest import Base
 
 
 @pytest.mark.webtest
-class TestID0015(Base):
+class TestID0016(Base):
     """
-        TestCase: 0014(1, 2)
+        TestCase: 0016(1, 2)
         Author: Iryna Vaskiv
         Description:
-            1. Check if user can see all brands into Shafa.ua site. Should use
-             'Вся женская одежда' button inside 'Женщинам' block
-            2. Check if brands have all elements(21)
+            1. Check if user can see all accessories into Shafa.ua site.
+             Should use 'Вся женская одежда' button inside 'Женщинам' block
+            2. Check if brands have all elements (13)
         Pre-condition: Login inside web interface.
         Steps to reproduce:
             1. Click in 'Женщинам' button
             2. Click in 'Все аксессуары' button
             3. Check the header inside page
         Expected result:
-            1. Should be 'Все аксессуары ' header.
-            2. Should be 12 elements.
+            1. Should be 'Женские аксессуары' header.
+            2. Should be 13 elements.
     """
 
-    def test_all_women_site(self):
+    def test_all_accessories(self):
         self.for_women_button().click()
         self.all_accessories().click()
         sleep(5)
-        all_women_header = self.selenium.find_element_by_xpath(
+        all_accessories_header = self.selenium.find_element_by_xpath(
             '/html/body/div/div[2]/div[2]/div/div/div/div/div[1]/h1')
-        set_get_accessories_header = set(all_women_header.text.split('\n'))
+        set_get_accessories_header = \
+            set(all_accessories_header.text.split('\n'))
         set_words = {'Женские аксессуары'}
         assert set_words == set_get_accessories_header
 
-    def test_all_women_elements(self):
+    def test_all_accessories_elements(self):
         self.for_women_button().click()
         self.all_accessories().click()
         sleep(5)
