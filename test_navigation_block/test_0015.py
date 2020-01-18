04608753ddee23d2da8d2acb6b2f439b2f866e74
irynavaskiv <irynavaskiv875@gmail.com>
2020-01-19 00:54:31 +0200

    fix 15 test

--- test_navigation_block/test_0015.py
+++ test_navigation_block/test_0015.py
@@ -7,11 +7,11 @@ import pytest
 @pytest.mark.webtest
 class TestID0015(Base):
     """
-        TestCase: 0014(1, 2)
+        TestCase: 0015(1, 2)
         Author: Iryna Vaskiv
         Description:
-            1. Check if user can see all brands into Shafa.ua site. Should use
-             'Вся женская одежда' button inside 'Женщинам' block
+            1. Check if user can see all elements into Shafa.ua site.
+             Should use 'Вся женская одежда' button inside 'Женщинам' block
             2. Check if brands have all elements(21)
         Pre-condition: Login inside web interface.
         Steps to reproduce:
@@ -29,17 +29,17 @@ class TestID0015(Base):
         sleep(5)
         all_women_header = self.selenium.find_element_by_xpath(
             '/html/body/div/div[2]/div[2]/div/div/div/div/div[1]/h1')
-        set_get_brands_header = set(all_women_header.text.split('\n'))
+        set_get_women_header = set(all_women_header.text.split('\n'))
         set_words = {'Женская одежда'}
-        assert set_words == set_get_brands_header
+        assert set_words == set_get_women_header
 
     def test_all_women_elements(self):
         self.for_women_button().click()
         self.all_woman_close().click()
         sleep(5)
-        container_brands = self.selenium.find_element_by_xpath(
+        container_elements = self.selenium.find_element_by_xpath(
             '/html/body/div/div[2]/div[2]/div/div/div/div/div[2]')
-        child_elements = container_brands. \
+        child_elements = container_elements. \
             find_elements_by_class_name('b-subcategories__item')
         if len(child_elements) != 21:
             pytest.skip(msg='Page do not have all elements')
