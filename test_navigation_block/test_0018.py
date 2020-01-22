3d7f5f2406bf6cd80fb5347d890200561fabd3af
irynavaskiv <irynavaskiv875@gmail.com>
2020-01-22 21:54:44 +0200

    add 18(4) test

--- test_navigation_block/test_0018.py
+++ test_navigation_block/test_0018.py
@@ -52,3 +52,14 @@ class TestID0018(Base):
             'div[2]')
         numbers = re.findall('(\d+)', item_part.text)
         assert int(item_text_number[0]) == int(numbers[0])
+
+    def test_if_exist_size_filter(self):
+        self.novelty().click()
+        size_block = self.selenium.find_element_by_xpath(
+            '/html/body/div/div[2]/div[2]/div/aside/div/'
+            'div/div[2]/div/div[1]/ul')
+        set_get_size_block_words = set(size_block.text.split('\n'))
+        set_words = {'Другой', '50/5XL', '54/7XL', '40/L', '46/3XL', '44/XXL',
+                     '38/M', '48/4XL', 'One size', '32/XXS', '52/6XL', '42/XL',
+                     '36/S', '34/XS'}
+        assert set_words == set_get_size_block_words
