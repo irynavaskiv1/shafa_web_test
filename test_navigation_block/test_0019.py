fce3ff3bcf29fd2c59a15caa8f910e5692e14cda
irynavaskiv <irynavaskiv875@gmail.com>
2020-01-25 23:07:26 +0200

    add 19 test

---
+++ test_navigation_block/test_0019.py
@@ -0,0 +1,32 @@
+import pytest
+
+from sf_setup_helper.base_pytest import Base
+
+
+@pytest.mark.webtest
+class TestID0019(Base):
+    """
+        TestCase: 0019(1)
+        Author: Iryna Vaskiv
+        Description:
+            1. Check if user can see all items into Shafa.ua site.
+             Should use 'Новинки' button inside 'Женщинам' block
+            2. Check if brands have all elements
+        Pre-condition: Login inside web interface.
+        Steps to reproduce:
+            1. Click in 'Новинки' button
+            2. Check the header inside page
+        Expected result:
+            1. 'Новинки' should have filter block with brands items
+    """
+
+    def test_if_brands_items_exist(self):
+        self.novelty().click()
+        size_block = self.selenium.find_element_by_xpath(
+            '/html/body/div[1]/div[2]/div[2]/div/aside/div'
+            '/div/div[2]/div/div[5]')
+        set_get_size_block_words = set(size_block.text.split('\n'))
+        set_words = {'George', 'Next', 'Atmosphere', 'New Look',
+                     "Victoria's Secret", 'Италия', 'Mango', 'H&M',
+                     'Бренд', 'Marks & Spencer', 'ZARA'}
+        assert set_words == set_get_size_block_words
