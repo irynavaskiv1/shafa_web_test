d0f53ec63458dd14ff62df78fb73f65119d1eae3
irynavaskiv <irynavaskiv875@gmail.com>
2020-01-25 23:47:47 +0200

    add 19-2 test

--- test_navigation_block/test_0019.py
+++ test_navigation_block/test_0019.py
@@ -6,7 +6,7 @@ from sf_setup_helper.base_pytest import Base
 @pytest.mark.webtest
 class TestID0019(Base):
     """
-        TestCase: 0019(1)
+        TestCase: 0019(1, 2)
         Author: Iryna Vaskiv
         Description:
             1. Check if user can see all items into Shafa.ua site.
@@ -18,15 +18,26 @@ class TestID0019(Base):
             2. Check the header inside page
         Expected result:
             1. 'Новинки' should have filter block with brands items
+            2. Sorting block should have all items
     """
 
     def test_if_brands_items_exist(self):
         self.novelty().click()
         size_block = self.selenium.find_element_by_xpath(
-            '/html/body/div[1]/div[2]/div[2]/div/aside/div'
-            '/div/div[2]/div/div[5]')
+            '/html/body/div[1]/div[2]/div[2]/div/aside/div/'
+            'div/div[2]/div/div[5]/ul')
         set_get_size_block_words = set(size_block.text.split('\n'))
         set_words = {'George', 'Next', 'Atmosphere', 'New Look',
                      "Victoria's Secret", 'Италия', 'Mango', 'H&M',
                      'Бренд', 'Marks & Spencer', 'ZARA'}
+        assert set_words == set_get_size_block_words
+
+    def test_if_sorting_block_exist(self):
+        self.novelty().click()
+        size_block = self.selenium.find_element_by_xpath(
+            '/html/body/div[1]/div[2]/div[2]/div/div/div'
+            '/div/div[4]/div[2]/span')
+        set_get_size_block_words = set(size_block.text.split('\n'))
+        set_words = {'по дате добавления', 'по популярности',
+                     'от дорогих к дешевым', 'от дешевых к дорогим'}
         assert set_words == set_get_size_block_words
