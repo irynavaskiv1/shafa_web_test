9dd8c92e4b20d02b9100cac28c7cbc4c5f364742
irynavaskiv <irynavaskiv875@gmail.com>
2020-01-29 22:44:14 +0200

    add new methods to helpers

--- sf_setup_helper/base_pytest.py
+++ sf_setup_helper/base_pytest.py
@@ -51,20 +51,80 @@ class Base:
 
     def sorting_block(self):
         return self.selenium.find_element_by_xpath(
-        '/html/body/div[1]/div[2]/div[2]/div/div/div/div/'
-        'div[4]/div[2]/span/select')
+            '/html/body/div[1]/div[2]/div[2]/div/div/div/div/'
+            'div[4]/div[2]/span/select')
 
     def for_kids(self):
         return self.selenium.find_element_by_xpath(
             '/html/body/div/div[2]/div[1]/div/div[1]/nav/div/ul/li[3]/a')
 
+    def all_girls_clothes(self):
+        return self.selenium.find_element_by_xpath(
+            '/html/body/div/div[2]/div[1]/div/div[1]/nav/div/ul/li[3]'
+            '/div/div[2]/div[1]/ul/li[1]/a')
+
+    def all_baby_clothes(self):
+        return self.selenium.find_element_by_xpath(
+            '/html/body/div/div[2]/div[1]/div/div[1]/nav/div/ul/li[3]'
+            '/div/div[2]/div[1]/ul/li[2]/a')
+
+    def all_boys_clothes(self):
+        return self.selenium.find_element_by_xpath(
+            '/html/body/div/div[2]/div[1]/div/div[1]/nav/div/ul/li[3]'
+            '/div/div[2]/div[2]/ul/li/a')
+
+    def all_the_clothes_for_the_kids(self):
+        return self.selenium.find_element_by_xpath(
+            '/html/body/div/div[2]/div[1]/div/div[1]/nav/div/ul/li[3]'
+            '/div/div[2]/div[3]/ul/li/a')
+
     def for_men(self):
         return self.selenium.find_element_by_xpath(
             '/html/body/div/div[2]/div[1]/div/div[1]/nav/div/ul/li[4]/a')
 
+    def all_men_clothing(self):
+        return self.selenium.find_element_by_xpath(
+            '/html/body/div/div[2]/div[1]/div/div[1]/nav/div/ul/li[4]'
+            '/div/div[2]/div[1]/ul/li/a')
+
+    def all_men_shoes(self):
+        return self.selenium.find_element_by_xpath(
+            '/html/body/div/div[2]/div[1]/div/div[1]/nav/div/ul/li[4]'
+            '/div/div[2]/div[2]/ul/li/a')
+
+    def all_men_accessories(self):
+        return self.selenium.find_element_by_xpath(
+            '/html/body/div/div[2]/div[1]/div/div[1]/nav/div/ul/li[4]'
+            '/div/div[2]/div[3]/ul/li/a')
+
     def for_home(self):
         return self.selenium.find_element_by_xpath(
             '/html/body/div/div[2]/div[1]/div/div[1]/nav/div/ul/li[5]/a')
+
+    def all_home_textiles(self):
+        return self.selenium.find_element_by_xpath(
+            '/html/body/div[1]/div[2]/div[1]/div/div[1]/nav/div/ul/li[5]'
+            '/div/div[2]/div[1]/ul/li[1]/a')
+
+    def all_utensils(self):
+        return self.selenium.find_element_by_xpath(
+            '/html/body/div/div[2]/div[1]/div/div[1]/nav/div/ul/li[5]'
+            '/div/div[2]/div[2]/ul/li/a')
+
+    def all_for_storing_things(self):
+        return self.selenium.find_element_by_xpath(
+            '/html/body/div/div[2]/div[1]/div/div[1]/nav/div/ul/li[5]'
+            '/div/div[2]/div[3]/ul/li/a')
+
+    def all_the_decor(self):
+        return self.selenium.find_element_by_xpath(
+            '/html/body/div/div[2]/div[1]/div/div[1]/nav/div/ul/li[5]'
+            '/div/div[2]/div[4]/ul/li/a')
+
+    def everything_for_home(self):
+        return self.selenium.find_element_by_xpath(
+            '/html/body/div/div[2]/div[1]/div/div[1]/nav/div/ul/li[5]'
+            '/div/div[2]/div[1]/ul/li[2]/a')
 
     def discount_day(self):
         return self.selenium.find_element_by_xpath(
