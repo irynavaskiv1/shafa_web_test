bf165f75034a12197f47c6258c9c62c9ee694ac4
irynavaskiv <irynavaskiv875@gmail.com>
2020-02-02 18:13:05 +0200

    update description and test to 20 scope

--- test_all_inside/test_0020.py
+++ test_all_inside/test_0020.py
@@ -6,20 +6,25 @@ from sf_setup_helper.base_pytest import Base
 @pytest.mark.webtest
 class TestID0019(Base):
     """
-        @TestCase: 0020
+        @TestCase: 0020 (1, 2, 3 )
         @Author: Iryna Vaskiv
         Description:
             1. Check if user can see all items into Shafa.ua site.
              Should use 'Для дома' button.
             2. Check if user can see all items into Shafa.ua site.
              Should use 'Для дома' button.
+            3. Check if price block have all items.
+            (For ex:'От 300 до 500 грн', 'Более 1000 грн', 'До 100 грн',
+                    'От 100 до 300 грн', 'От', 'Цена',  'До',)
         Pre-condition: Login inside web interface.
         Steps to reproduce:
             1. Click in 'Для дома' button
             2. Check the section container inside page
+            3. Check the all items inside 'Цена' block
         Expected result:
             1. 'Для дома' should have section container block
             2. Container block should have name of all items
+            3. Container block should have name of all items
     """
 
     def test_if_first_main_inner_exist(self):
@@ -28,7 +33,7 @@ class TestID0019(Base):
         all_item = self.selenium.find_element_by_xpath(
             '/html/body/div[1]/div[2]/div[2]/div/aside/div[1]/ul')
         set_all_words = set(all_item.text.split('\n'))
-        set_all_text = {'Shafa.ua', 'Домашний текстиль', 'Для дома'}
+        set_all_text = {'Shafa.ua', 'Для дома', 'Домашний текстиль'}
         assert set_all_words == set_all_text
 
     def test_if_second_main_inner_exist(self):
