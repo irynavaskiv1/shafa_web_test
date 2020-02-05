aca0e701e82cfa492d1fd0f99985bd9ce1e2553b
irynavaskiv <irynavaskiv875@gmail.com>
2020-02-05 21:24:03 +0200

    add third test to 21 scope

--- test_checking_colors/test_0021.py
+++ test_checking_colors/test_0021.py
@@ -1,8 +1,8 @@
 import re
 from time import sleep
+import webcolors
 
 import pytest
-import webcolors
 
 from sf_setup_helper.base_pytest import Base
 
@@ -14,7 +14,7 @@ class TestID0019(Base):
         self.for_home().click()
         self.all_home_textiles().click()
         elements = self.selenium.find_elements_by_class_name(
-            "b-color-list__label")
+            'b-color-list__label')
         assert len(elements) == 24
 
     def test_white_color(self):
@@ -29,3 +29,17 @@ class TestID0019(Base):
         rgb_clear = [int(i) for i in rgb_list]
         name_color = webcolors.rgb_to_name(rgb_clear)
         assert name_color == 'white'
+
+    def test_check_all_colors(self):
+        self.for_home().click()
+        self.all_home_textiles().click()
+        sleep(1)
+        elements = self.selenium.find_elements_by_class_name(
+            'b-filter__content b-color-list')
+        for element in elements:
+            full_rgb = element.value_of_css_property('background-color')
+            rgb_list = list(
+                re.search(r'rgb\((\d+),\s*(\d+),\s*(\d+)', full_rgb).groups())
+            rgb_clear = [int(i) for i in rgb_list]
+            name_color = webcolors.rgb_to_name(rgb_clear)
+            assert type(name_color) == str
