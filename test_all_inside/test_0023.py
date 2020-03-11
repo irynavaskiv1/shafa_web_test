faaf8b0bf48cc99ef2f0f1d6f02a0429dd31ba42
irynavaskiv <irynavaskiv875@gmail.com>
2020-03-11 23:23:54 +0200

    add new test

--- test_all_inside/test_0023.py
+++ test_all_inside/test_0023.py
@@ -18,25 +18,27 @@ class TestID0023(Base):
             1. Click in 'Search' button
             2. Enter the value to search block
             3. Check the value is shows correct
+            4. Check the value inside combobox
         Expected result:
             1. Value in second page is the same
+            2. Check the value inside combobox is the same
     """
 
-    # def test_search_func(self):
-    #     search_input = self.selenium.find_element_by_xpath(
-    #         '/html/body/div/div[2]/div[1]/div/'
-    #         'div[1]/nav/div/form/input')
-    #     search_input.click()
-    #     random_value = random.choice(
-    #         ['Піджак', 'Жакет', 'Спідниця', 'Штани', 'Шорти'])
-    #     search_input.send_keys(random_value)
-    #     search_input.send_keys(u'\ue007')
-    #     sleep(5)
-    #     catalog_search_block = self.selenium.find_element_by_xpath(
-    #         '/html/body/div/div[2]/div[2]/div/div'
-    #         '/div/div/div[1]/form/div[1]/input')
-    #     input_value_second = catalog_search_block.get_attribute("value")
-    #     assert random_value == input_value_second
+    def test_search_func(self):
+        search_input = self.selenium.find_element_by_xpath(
+            '/html/body/div/div[2]/div[1]/div/'
+            'div[1]/nav/div/form/input')
+        search_input.click()
+        random_value = random.choice(
+            ['Піджак', 'Жакет', 'Спідниця', 'Штани', 'Шорти'])
+        search_input.send_keys(random_value)
+        search_input.send_keys(u'\ue007')
+        sleep(5)
+        catalog_search_block = self.selenium.find_element_by_xpath(
+            '/html/body/div/div[2]/div[2]/div/div'
+            '/div/div/div[1]/form/div[1]/input')
+        input_value_second = catalog_search_block.get_attribute("value")
+        assert random_value == input_value_second
 
     def test_search_block_item(self):
         search_input = self.selenium.find_element_by_xpath(
