d6d9a85581ac136c17754d26842594c6b1dad7ba
irynavaskiv <irynavaskiv875@gmail.com>
2020-03-28 22:40:08 +0200

    add 27 test

--- test_all_inside/test_0027.py
+++ test_all_inside/test_0027.py
@@ -6,11 +6,12 @@ from sf_setup_helper.base_pytest import Base
 @pytest.mark.webtest
 class TestID0027(Base):
     """
-        @TestCase: 0027 (1)
+        @TestCase: 0027 (1, 2)
         @Author: Iryna Vaskiv
         Description:
             1. Check if items inside block have all elements. (Check if all
                 children ids have name, price, like, size)
+            2. Check if items inside block have 'грн' word.
         Pre-condition: Login inside web/mobile interface.
         Steps to reproduce:
             1. Click in 'Discount' button
@@ -18,9 +19,10 @@ class TestID0027(Base):
             3. Made iteration inside block (check all items)
         Expected result:
             1. Value in page should have all items and have the same len
+            2. All items have 'грн' word
     """
 
-    def test_rubric_inside_discount_day_block(self):
+    def test_item_inside_block_first(self):
         self.discount_day().click()
         self.women_clothes().click()
         html_list = self.selenium.find_element_by_xpath(
@@ -30,3 +32,13 @@ class TestID0027(Base):
         assert len(is_children_ids_exist) > 0
         result_each_item = [True for i in is_children_ids_exist if len(i) > 0]
         assert all(result_each_item) == True
+
+    def test_item_inside_block_second(self):
+        self.discount_day().click()
+        self.women_clothes().click()
+        html_list = self.selenium.find_element_by_xpath(
+            '/html/body/div/div[2]/div[2]/div/div/div/div/ul')
+        children_ids = html_list.find_elements_by_tag_name("li")
+        is_uan_exist_in_each_item = \
+            [True for i in children_ids if 'грн' in i.text]
+        assert all(is_uan_exist_in_each_item) == True
