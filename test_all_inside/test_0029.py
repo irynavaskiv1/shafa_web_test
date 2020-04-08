fd5f74db435220eb549eb78cfe51d7a5e3aaa048
irynavaskiv <irynavaskiv875@gmail.com>
2020-04-08 22:56:33 +0300

    add description step 29 test

--- test_all_inside/test_0029.py
+++ test_all_inside/test_0029.py
@@ -4,9 +4,23 @@ from sf_setup_helper.base_pytest import Base
 
 
 @pytest.mark.webtest
-class TestID0028(Base):
+class TestID0029(Base):
+    """
+        @TestCase: 0029 (1)
+        @Author: Iryna Vaskiv
+        Description:
+            1. Check if items inside main block have all fields.
+        Pre-condition: Login inside web/mobile interface.
+        Steps to reproduce:
+            1. Click shafa.ua site
+            2. Select block items inside main page
+            3. Get children ids
+        Expected result:
+            1. Children ids should have all correct items
+    """
 
     def test_items_inside_mail_block(self):
+        """ for vip-revelation"""
         html_list = self.selenium.find_element_by_xpath(
             '/html/body/div/div[2]/div[4]/div/ul[1]')
         children_ids = html_list.find_elements_by_tag_name("li")
